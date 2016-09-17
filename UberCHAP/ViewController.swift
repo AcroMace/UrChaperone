c757a175d037a86e461c111cef61dad248d90c69
Andy Cho <acromace@gmail.com>
2016-09-17 04:18:12 -0400

    Initial Commit

---
+++ UberCHAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UberCHAP
+//
+//  Created by Andy Cho on 2016-09-17.
+//  Copyright © 2016 Students Concerned About Party Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
